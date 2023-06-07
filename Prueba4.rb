a2c06ebdb16c29a5fd8ed83d1ee1efc526702efd
Nathalie Moraga <129231844+NatieMor@users.noreply.github.com>
2023-06-07 14:46:37 -0400

    actualizado Prueba4.rb
    se arreglo requerimiento 1 y 2

--- Prueba4.rb
+++ Prueba4.rb
@@ -10,27 +10,7 @@ class Person
     def birthday
     @age += 1
     end
-    def talk
-    if @type == "Student"
-    puts "Aquí es la clase de programación con Ruby?"
-    elsif @type == "Teacher"
-    puts "Bienvenidos a la clase de programación con Ruby!"
-    elsif @type == "Parent"
-    puts "Aquí es la reunión de apoderados?"
-    end
-    end
-    def introduce
-        if @type == "Student"
-        puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
-        elsif @type == "Teacher"
-        puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
-        elsif @type == "Parent"
-        puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name}
-        #{@last_name}."
-        end
-        end
-        end
-
+end
 class Student < Person
     def talk
         puts "Aquí es la clase de programación con Ruby?"
@@ -40,36 +20,24 @@ class Student < Person
     end
 end
 class Teacher < Person
-
-    def talk
+     def talk
         puts "Bienvenidos a la clase de programación con Ruby!"
-        end
-    end
-    def introduce
-            if @type == "Student"
-            puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
-    end
+     end
+     def introduce
+       puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+     end
 end
 class Parent < Person
     def talk
         puts "Aquí es la reunión de apoderados?"
-        end
     end
     def introduce
-        puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #{@last_name}."
-        end
+        puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #{@last_name}."   
     end
 end
-        #Utilizando los principios de Herencia y Polimorfismo, refactoriza el siguiente código para
-#eliminar la innecesaria cantidad de sentencias if en los métodos talk e introduce
-Requerimientos
-1. Identificar las posibles subclases que aparecen en el código a refactorizar. Cada una
-de ella contendrá los métodos correspondientes, talk e introduce.
-(3 Puntos)
-Hint: Teacher sería una clase.
-2. A partir de la refactorización del código se debe generar herencia de ancestros
-aplicando la sintaxis de Ruby, esto para la subclases.
-(3 Puntos)
+Talk
+introduce 
+
 3. El método introduce al imprimir el mensaje muestra el nombre y apellido haciendo
 uso de interpolación de variables de instancia.
 (1 Puntos)
